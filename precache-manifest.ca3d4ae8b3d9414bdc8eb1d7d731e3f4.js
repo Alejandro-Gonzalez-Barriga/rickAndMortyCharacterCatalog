2757bcf0232bd602bcf1cc7e0469412317a85aa7
alexG <alejandro.gonzalez.barriga@gmail.com>
2020-01-14 13:35:32 -0600

    Updates

---
+++ precache-manifest.ca3d4ae8b3d9414bdc8eb1d7d731e3f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b84c27c4261ec1da3c5b6e42d56fb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3c2d3eb108dcc36a858",
+    "url": "/static/css/main.91d128c4.chunk.css"
+  },
+  {
+    "revision": "2bf4227568b8cc2afc96",
+    "url": "/static/js/2.8df6b6b5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8df6b6b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3c2d3eb108dcc36a858",
+    "url": "/static/js/main.e7d2fa0d.chunk.js"
+  },
+  {
+    "revision": "d11d66b7f16e222a2423",
+    "url": "/static/js/runtime-main.7dd024e7.js"
+  },
+  {
+    "revision": "42d6efdb5571c7fc125c19f9d1ab2b3b",
+    "url": "/static/media/logo.42d6efdb.png"
+  }
+]);
